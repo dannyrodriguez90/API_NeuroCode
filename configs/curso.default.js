59ce6eaa46ef15385e395f7cb10a6fa1e290aad7
dannyrodriguez90 <rmdanny889@gmail.com>
2025-05-11 15:29:22 -0600

    se solucionan problemas con cursos y se actualiza la data

--- configs/curso.default.js
+++ configs/curso.default.js
@@ -1,21 +1,31 @@
 import Curso from "../src/curso/curso.model.js";
+import mongoose from "mongoose";
 
 export const inicializarCursos = async () => {
     try {
         const cursosPorDefecto = [
-            { nombre: "Taller III", descripcion: "Curso de Taller III" },
-            { nombre: "Tecnología III", descripcion: "Curso de Tecnología III" },
-            { nombre: "Prácticas Supervisadas", descripcion: "Curso de Prácticas Supervisadas" }
+            { 
+                _id: new mongoose.Types.ObjectId("68210790ea080f65da4a1c23"),
+                nombre: "Taller III", 
+                descripcion: "Curso de Taller III" 
+            },
+            { 
+                _id: new mongoose.Types.ObjectId("68210790ea080f65da4a1c27"),
+                nombre: "Tecnología III", 
+                descripcion: "Curso de Tecnología III" 
+            },
+            { 
+                _id: new mongoose.Types.ObjectId("08210790ea080f65da4a1c2a"),
+                nombre: "Prácticas Supervisadas", 
+                descripcion: "Curso de Prácticas Supervisadas" 
+            }
         ];
 
         for (const curso of cursosPorDefecto) {
-            const existeCurso = await Curso.findOne({ nombre: curso.nombre });
-            if (!existeCurso) {
-                await Curso.create(curso);
-                console.log(`Curso creado: ${curso.nombre}`);
-            } else {
-                console.log(`El curso "${curso.nombre}" ya existe.`);
-            }
+            await Curso.deleteOne({ nombre: curso.nombre });
+
+            await Curso.create(curso);
+            console.log(`Curso creado: ${curso.nombre}`);
         }
     } catch (err) {
         console.error("Error al inicializar los cursos por defecto:", err.message);
